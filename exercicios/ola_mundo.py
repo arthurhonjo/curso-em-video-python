444f5a893ca963f606235ffdebc5f72a10740e2f
Arthur Iogue Honjo <56140690+arthurhonjo@users.noreply.github.com>
2021-04-27 17:34:38 -0300

    Update ola_mundo.py

--- exercicios/ola_mundo.py
+++ exercicios/ola_mundo.py
@@ -1,11 +1,1 @@
-Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print('Olá Mundo!')
-Olá Mundo!
->>> print(4+5)
-9
->>> print('4'+'9')
-49
->>> print('Olá', 4)
-Olá 4
->>> 
+print('Olá Mundo!')
